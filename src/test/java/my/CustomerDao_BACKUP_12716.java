f36aa965bf40fd35f0aee783067252cbe98090ce
wyyjida <wyyjida@163.com>
2016-10-12 11:37:50 +0800

    no message

--- src/test/java/my/CustomerDao_BACKUP_12716.java
+++ src/test/java/my/CustomerDao_BACKUP_12716.java
@@ -1,10 +1,12 @@
 package my;
 
-/**
  * Created by Acting on 2016/10/11.
  */
 public interface CustomerDao {
+<<<<<<< HEAD
     void delete(String id);
+=======
     void save();
+>>>>>>> master
     void delete();
 }
